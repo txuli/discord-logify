2d682c4c755a43c5abbdf44c699e5122a24cb80e
txuli <127785824+txuli@users.noreply.github.com>
2026-02-03 23:08:41 +0000

    feat: implement CLI for adding custom logs and update logging functionality

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,9 +1,44 @@
 #!/usr/bin/env node
-const args = process.argv.slice(2);
+import { Command } from 'commander';
+import { log } from '../src/index.ts';
+import fs from "fs"
+const program = new Command();
+const logger = new log()
+interface AddLogOptions {
+    color?: boolean;
+}
+/* prefix:string, logColor:string, logTag:string */
+program
+    .name('logger-cli')
+    .description('CLI para gestionar logs personalizados');
+
+program
+    .command('add-log')
+    .description('Añade un log personalizado')
+    .argument('<logName>', 'logs name')
+    .requiredOption('-c, --color <string>')
+    .requiredOption('-t, --logTag <string>')
+    .action((logName, options) => {
+    logger.addLogLevel(logName, options.color, options.logTag)
+  });
+
+
+program.parse();
+
+
+
+
+
+
+
+/* const args = process.argv.slice(2);
 console.log(args)
 const command = args[0];
+const options = program.opts();
+const limit = options.color ? 1 : undefined;
+console.log(program.args[0].split(options.separator, limit)); */
 
-function getFlag(flag:string) {
+/* function getFlag(flag:string) {
   const index = process.argv.indexOf(flag);
   if (index === -1) return undefined;
   return process.argv[index + 1];
@@ -19,4 +54,6 @@ if (command === "add-log") {
 }
 
 console.log("Comando no reconocido");
-process.exit(1);
+process.exit(1); */
+
+//TODO buscar info commander si es veneficioso 
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,60 @@
-
+import fs from 'node:fs';
+import path from 'node:path';
+import os from 'node:os';
 export type logLevel = {
     color: string;
     prefix: string;
-
+    text: string
 }
 export class log {
+    private configPath = path.join(process.cwd(), 'logger-config.json');
+    private level = new Map<string, logLevel>([
 
-    private level = new Map<string, logLevel>([])
+    ])
+    constructor() {
+        this.loadFromFile()
+        console.log("a")
+    }
+    private loadFromFile() {
+        if (fs.existsSync(this.configPath)) {
+            const data = fs.readFileSync(this.configPath, 'utf-8');
+            const parsedData = JSON.parse(data) as Record<string, logLevel>;
+            this.level = new Map(Object.entries(parsedData));
+            console.log(this.level)
+        }
+    }
+    private date() {
+        const d = new Date();
+        const day = String(d.getDate()).padStart(2, '0');
+        const month = String(d.getMonth() + 1).padStart(2, '0'); // +1 porque enero = 0
+        const year = d.getFullYear();
+
+        const hour = String(d.getHours()).padStart(2, '0');
+        const min = String(d.getMinutes()).padStart(2, '0');
+        const sec = String(d.getSeconds()).padStart(2, '0');
+
+        return `[${day}/${month}/${year} ${hour}:${min}:${sec}]`;
+        ;
+    }
     Info(msg: string) {
-        process.stdout.write(`${date()} \x1b[36m[INFO]\x1b[0m ${msg}\n`);
+        process.stdout.write(`${this.date()} \x1b[36m[INFO]\x1b[0m ${msg}\n`);
     }
-    Alert(msg:string){
-         process.stdout.write(`${date()} \x1b[33m[WARN]\x1b[0m ${msg}\n`);
+    Alert(msg: string) {
+        process.stdout.write(`${this.date()} \x1b[33m[WARN]\x1b[0m ${msg}\n`);
     }
-    Error(msg:string){
-         process.stdout.write(`${date()} \x1b[31m[ERROR]\x1b[0m ${msg}\n`);
+    Error(msg: string) {
+        process.stdout.write(`${this.date()} \x1b[31m[ERROR]\x1b[0m ${msg}\n`);
+    }
+    public addLogLevel(prefix: string, logColor: string, logTag: string): void {
+        /* if(!fs.existsSync(this.configPath)){ */
+           
+            this.level.set(prefix, { color: logColor, prefix: prefix, text: logTag })
+            
+            fs.writeFileSync(this.configPath, JSON.stringify(Object.fromEntries(this.level)), 'utf-8');
+      /*   } */
+        
+        
     }
 }
-function date() {
-    const d = new Date();
-    const day = String(d.getDate()).padStart(2, '0');
-    const month = String(d.getMonth() + 1).padStart(2, '0'); // +1 porque enero = 0
-    const year = d.getFullYear();
 
-    const hour = String(d.getHours()).padStart(2, '0');
-    const min = String(d.getMinutes()).padStart(2, '0');
-    const sec = String(d.getSeconds()).padStart(2, '0');
 
-    return `[${day}/${month}/${year} ${hour}:${min}:${sec}]`;
-    ;
-}
